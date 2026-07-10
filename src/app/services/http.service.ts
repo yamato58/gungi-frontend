0f6580234d2d223a86faa4bc8dc62fd83c6062bd
yamato58 <boruto.popo8010@gmail.com>
2026-07-10 16:32:57 +0900

    Update http.service.ts

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -14,26 +14,26 @@ import { MoveResult } from '../models/MoveResult.model';
 export class HttpService {
     // HttpClient(getやpostをするのに必要な機能)の注入
     private http = inject(HttpClient);
-    const apiUrl = 'https://gungi-backend.onrender.com';
+    // const apiUrl = 'https://gungi-backend.onrender.com';
     
     // C#側にリクエスト
     getInitialPieces(): Observable<MoveResult> {
-        return this.http.get<MoveResult>('${apiUrl}/game/initial-data');
+        return this.http.get<MoveResult>('https://gungi-backend.onrender.com/game/initial-data');
     }
 
     postSelectedPieces(postdata: Piece): Observable<CalculatedData[]> {
-        return this.http.post<CalculatedData[]>('${apiUrl}/game/select-data', postdata);
+        return this.http.post<CalculatedData[]>('https://gungi-backend.onrender.com/game/select-data', postdata);
     }
 
     postSelectedCell(movePiece: MovePiece): Observable<MoveResult> {
-        return this.http.post<MoveResult>('${apiUrl}/game/next-data', movePiece);
+        return this.http.post<MoveResult>('https://gungi-backend.onrender.com/game/next-data', movePiece);
     }
 
     postClickedBoardReset(): Observable<MoveResult> {
-        return this.http.post<MoveResult>('${apiUrl}/game/boardreset-data', null);
+        return this.http.post<MoveResult>('https://gungi-backend.onrender.com/game/boardreset-data', null);
     }
 
     postClickedCellReset(): Observable<CalculatedData[]> {
-        return this.http.post<CalculatedData[]>('${apiUrl}/game/cellreset-data', null);
+        return this.http.post<CalculatedData[]>('https://gungi-backend.onrender.com/game/cellreset-data', null);
     }
 }
