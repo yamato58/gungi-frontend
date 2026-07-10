9817ab08188c9870e86b9ae1acf46739a77eaf4e
yamato58 <boruto.popo8010@gmail.com>
2026-07-10 16:26:38 +0900

    Update http.service.ts

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,11 +2,9 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Piece } from '../models/piece.model';
-// import { SelectPiece } from '../models/selectPiece.model';
 import { CalculatedData } from '../models/calculatedData.model';
 import { MovePiece } from '../models/movePiece.model';
 import { MoveResult } from '../models/MoveResult.model';
-// import { GameJudge } from '../models/MoveResult.model';
 
 // サービスをアプリケーション全体で共有し、自動的に依存性注入
 @Injectable({
@@ -16,25 +14,26 @@ import { MoveResult } from '../models/MoveResult.model';
 export class HttpService {
     // HttpClient(getやpostをするのに必要な機能)の注入
     private http = inject(HttpClient);
-
+    const apiUrl = 'https://gungi-backend.onrender.com';
+    
     // C#側にリクエスト
     getInitialPieces(): Observable<MoveResult> {
-        return this.http.get<MoveResult>('/api/game/initial-data');
+        return this.http.get<MoveResult>('${apiUrl}/game/initial-data');
     }
 
     postSelectedPieces(postdata: Piece): Observable<CalculatedData[]> {
-        return this.http.post<CalculatedData[]>('/api/game/select-data', postdata);
+        return this.http.post<CalculatedData[]>('${apiUrl}/game/select-data', postdata);
     }
 
     postSelectedCell(movePiece: MovePiece): Observable<MoveResult> {
-        return this.http.post<MoveResult>('/api/game/next-data', movePiece);
+        return this.http.post<MoveResult>('${apiUrl}/game/next-data', movePiece);
     }
 
     postClickedBoardReset(): Observable<MoveResult> {
-        return this.http.post<MoveResult>('/api/game/boardreset-data', null);
+        return this.http.post<MoveResult>('${apiUrl}/game/boardreset-data', null);
     }
 
     postClickedCellReset(): Observable<CalculatedData[]> {
-        return this.http.post<CalculatedData[]>('/api/game/cellreset-data', null);
+        return this.http.post<CalculatedData[]>('${apiUrl}/game/cellreset-data', null);
     }
 }
